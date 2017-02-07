d76c3ad50ced09880cc2f27a98556721955bcc7e
Anthony DePalatis <111tdepalatis@gmail.com>
2017-02-07 08:07:27 -0600

    Adding writeup and pynb files

--- Lab3/problem1abc.py
+++ Lab3/problem1abc.py
@@ -9,6 +9,7 @@ v2 = [0,1,0,1]
 v3 = [1,4,3,6]
 v4 = [2,11,6,15]
 A = Matrix([v1, v2, v3, v4])
+#print np.array(A)
 
 # (1)
 # Get the column vectors that span the column space (i.e., span(A))
@@ -16,7 +17,11 @@ S = A.columnspace()
 
 # Create a vector in span(A)
 # HELP
-vectorInS = S[0]
+random_vector = np.random.rand(1, 4)
+print random_vector
+print np.array(A)
+#thing = orth(np.array(A))
+#print thing
 
 # Create a vector not in span(A)
 # HELP
@@ -30,8 +35,8 @@ dim = len(S)
 # Convert S to a numpy array
 S_Array = [[], []] 
 for i in range(len(S)):
-    for j in range(len(vectorInS)):
+    for j in range(len(S[0])):
         S_Array[i].append([S[i][j]])
         
-print S_Array
-orthoBasis = orth(A)
+#print S_Array
+#orthoBasis = orth(A)
